70c8b793d9ac34123bb59cdbea0460ac96b8e91e
Dmitriy Petrov <d.petrov@inlinetelecom.ru>
2024-05-28 21:43:19 +0400

    added PhoneBook.containsPhoneNumber(); code refactoring

--- src/ru/otus/java/basic/homeworks/collections2/MainTask.java
+++ src/ru/otus/java/basic/homeworks/collections2/MainTask.java
@@ -2,14 +2,14 @@ package ru.otus.java.basic.homeworks.collections2;
 
 public class MainTask {
     public static void main(String[] args) {
-        PhoneBook.add("Вася", 79271234455L);
-        PhoneBook.add("Коля", 79271235566L);
-        PhoneBook.add("Коля", 79271235577L);
-        PhoneBook.add("Коля", 79271235588L);
-        PhoneBook.add("Петя", 79271236677L);
+        PhoneBook.add("Вася", "+79271234455");
+        PhoneBook.add("Коля", "+79271235566");
+        PhoneBook.add("Коля", "+79271235577");
+        PhoneBook.add("Коля", "+79271235588");
+        PhoneBook.add("Петя", "+79271236677");
 
         System.out.println(PhoneBook.find("Коля"));
+        System.out.println("Номер +79271236677 в справочнике - " + PhoneBook.containsPhoneNumber("+79271236677"));
         System.out.println("Всего контактов: " + PhoneBook.getContactsCount());
     }
-
 }
--- src/ru/otus/java/basic/homeworks/collections2/PhoneBook.java
+++ src/ru/otus/java/basic/homeworks/collections2/PhoneBook.java
@@ -3,36 +3,46 @@ package ru.otus.java.basic.homeworks.collections2;
 import java.util.*;
 
 public class PhoneBook {
-    private static Map<String, List<Long>> contacts;
+    private static Map<String, List<String>> contacts;
 
     static {
         contacts = new HashMap<>();
     }
 
-    public PhoneBook(){}
+    public PhoneBook() {
+    }
 
-    public static void add(String name, long phoneNumber){
-        if(contacts.containsKey(name)){
+    public static void add(String name, String phoneNumber) {
+        if (contacts.containsKey(name)) {
             contacts.get(name).add(phoneNumber);
         } else {
-            List<Long> numbers = new ArrayList<>();
+            List<String> numbers = new ArrayList<>();
             numbers.add(phoneNumber);
             contacts.put(name, numbers);
         }
     }
 
-    public static List<Long> find(String name){
-        List<Long> result = new ArrayList<>();
-        Set<Map.Entry<String, List<Long>>> entrySet = contacts.entrySet();
-        for(Map.Entry<String, List<Long>> entry : entrySet){
-            if(name.equals(entry.getKey())){
+    public static List<String> find(String name) {
+        List<String> result = new ArrayList<>();
+        Set<Map.Entry<String, List<String>>> entrySet = contacts.entrySet();
+        for (Map.Entry<String, List<String>> entry : entrySet) {
+            if (name.equals(entry.getKey())) {
                 result.addAll(entry.getValue());
             }
         }
         return result;
     }
 
-    public static int getContactsCount(){
+    public static boolean containsPhoneNumber(String phoneNumber) {
+        for (List<String> entry : contacts.values()) {
+            if (entry.contains(phoneNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int getContactsCount() {
         return contacts.size();
     }
 }
